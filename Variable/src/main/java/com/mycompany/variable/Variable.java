af61b15e72e5cf64043c34178e6a5d4879f701a7
Matias Ariel Coco <91424961+matiasCoco1997@users.noreply.github.com>
2025-10-31 15:30:56 -0300

    clase variables

---
+++ Variable/src/main/java/com/mycompany/variable/Variable.java
@@ -0,0 +1,21 @@
+package com.mycompany.variable;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        
+        int numero = 0;
+        String nombre = "Matias";
+        char letra = 'M';
+        double temperatura = 25.3;
+        boolean siONo = true;
+        long numeroLargo = 123456789;
+        
+        System.out.println("Numero = " + numero);
+        System.out.println("Nombre = " + nombre);
+        System.out.println("Letra = " + letra);
+        System.out.println("Temperatura = " + temperatura);
+        System.out.println("Booleano = " + siONo);
+        System.out.println("Numero largo = " + numeroLargo);
+    }
+}
