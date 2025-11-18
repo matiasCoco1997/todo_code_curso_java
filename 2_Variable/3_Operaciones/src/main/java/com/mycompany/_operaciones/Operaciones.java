05ab1f5a3676f4d736cd375a0c0b86fa51df0af5
Matias Ariel Coco <91424961+matiasCoco1997@users.noreply.github.com>
2025-11-18 09:15:54 -0300

    hasta clase intercambio de valores

---
+++ 2_Variable/3_Operaciones/src/main/java/com/mycompany/_operaciones/Operaciones.java
@@ -0,0 +1,39 @@
+package com.mycompany._operaciones;
+
+public class Operaciones {
+
+    public static void main(String[] args) {
+        
+        int numUno, numDos, resultado;
+        
+        numUno = 2;
+        numDos = 3;
+        
+        resultado = numUno + numDos;
+        
+        System.out.println("El resultado de la suma es: " + resultado);
+        
+        
+        numUno = 2;
+        numDos = 3;
+        
+        resultado = numUno - numDos;
+        
+        System.out.println("El resultado de la resta es: " + resultado);
+        
+        numUno = 4;
+        numDos = 2;
+        
+        resultado = numUno / numDos;
+        
+        System.out.println("El resultado de la división es: " + resultado);
+        
+        numUno = 4;
+        numDos = 2;
+        
+        resultado = numUno * numDos;
+        
+        System.out.println("El resultado del producto es: " + resultado);
+              
+    }
+}
