fc697013822cb5a0ede476138274bb06fa2b856f
Matias Ariel Coco <91424961+matiasCoco1997@users.noreply.github.com>
2025-11-25 10:07:54 -0300

    clase arrays

---
+++ 9_arrays/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    static void main() {
+        //Declaración de vectores
+        int vector [] = new int [5]; //sin definir el límite de posisiones
+
+        //Asignación (manual)
+        vector[0] = 10;
+        vector[1] = 20;
+        vector[2] = 30;
+        System.out.println(vector[1]);
+    }
+}
