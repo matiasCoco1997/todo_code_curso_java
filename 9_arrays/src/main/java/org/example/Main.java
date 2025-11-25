7e5f42c784d147e4b496b9da86b86a8405215f9f
Matias Ariel Coco <91424961+matiasCoco1997@users.noreply.github.com>
2025-11-25 10:33:15 -0300

    carga de vector por teclado y mostrar sus valores

--- 9_arrays/src/main/java/org/example/Main.java
+++ 9_arrays/src/main/java/org/example/Main.java
@@ -1,17 +1,31 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     static void main() {
         //Declaración de vectores
-        int vector [] = new int [5]; //sin definir el límite de posisiones
+        int vector [] = new int [3]; //sin definir el límite de posisiones
 
         //Asignación (manual)
+        /*
         vector[0] = 10;
         vector[1] = 20;
         vector[2] = 30;
+         */
 
-        System.out.println("Posicion [0]: " + vector[0]);
-        System.out.println("Posicion [1]: " + vector[1]);
-        System.out.println("Posicion [2]: " + vector[2]);
+        Scanner teclado = new Scanner(System.in);
+
+        //carga del vector
+        for(int i = 0; i < vector.length ; i++ ){
+            System.out.println("Ingrese el valor númerico para el índice: " + i);
+            vector[i] = teclado.nextInt();
+        }
+
+
+        //recorrido del vector
+        for(int i = 0; i < vector.length ; i++ ){
+            System.out.println("Posicion ["+ i +"]: " + vector[i]);
+        }
     }
 }
